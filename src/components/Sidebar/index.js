1a9479c441a8d6e3c0e467a2f94d0e297e3e0996
Yasar Budulgan <yasar.budulgan@vlmedia.com.tr>
2021-05-18 22:18:12 +0300

    Show algorithm list in sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { AlgorithmService } from 'algorithms';
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const { activeAlgorithm } = useSelector(state => state.app);
+  const algoNames = AlgorithmService.listAlgoNames();
+  
+  return (
+    <div>
+      <h1>
+        { activeAlgorithm }
+      </h1>
+      <ul>
+        {
+          algoNames.map(algo => <li key={algo.key}><Link to={`/${algo.key}`}>{algo.name}</Link></li>)
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
