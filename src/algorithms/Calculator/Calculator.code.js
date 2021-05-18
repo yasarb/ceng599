29b8f07bc7fea0722a603fcec3a93288b326ced7
Yasar Budulgan <yasar.budulgan@vlmedia.com.tr>
2021-05-18 22:15:46 +0300

    Add dummy component: Calculator

---
+++ src/algorithms/Calculator/Calculator.code.js
@@ -0,0 +1,15 @@
+const ConvexHull = `import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+ 
+import static java.util.Collections.emptyList;
+ 
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println('Hello World');
+    }
+
+}`;
+
+export default ConvexHull;
---
+++ src/algorithms/Calculator/Calculator.rd3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Calculator.scss';
+
+function CalculatorRenderer(props) {
+
+    return (
+      <p>Calculator</p>
+  );
+}
+
+export default CalculatorRenderer;
