348278b28f98498b5a3b0ed2dc91527c345dad0c
Yasar Budulgan <yb.budulgan@gmail.com>
2021-07-08 17:57:09 +0300

    Update home page readme

--- src/algorithms/Homepage/Readme.js
+++ src/algorithms/Homepage/Readme.js
@@ -4,5 +4,10 @@ module.exports = {
 ## METU CENG599 Term Project
 
 Yasar Budulgan - 2463941
+
+This website provides an interactive platform to visualize computational geometry algorithms.
+
+<img src="https://raw.githubusercontent.com/yasarb/ceng599/master/assets/img/home.png?token=AB3XJYJ7DWPXEEGZRWJ4QJDA6BFUS" width="700px"/>
+
 `,
 }
